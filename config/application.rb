ba822f410ed1df6afe9e97924ab223d21684434a
Joe Woodward <woodward.jp@gmail.com>
2012-05-30 23:06:09 +0100

    Configured RSpec.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ end
 
 module SamAndJamieWedding
   class Application < Rails::Application
+      config.generators do |generate|
+      generate.test_framework   :rspec, :fixture => true, :views => false
+      generate.integration_tool :rspec, :fixture => true, :views => true
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
