6cb7aa2e589eb8279fea65ae18b12d7b38ad53cf
Joe Woodward <woodward.jp@gmail.com>
2012-05-31 14:56:47 +0100

    add password to order form

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 3) do
+ActiveRecord::Schema.define(:version => 4) do
 
   create_table "orders", :force => true do |t|
     t.string   "type"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "password"
   end
 
   create_table "people", :force => true do |t|
